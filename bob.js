6fc77082e40e40d6d6e398612bae0faed6f8b84c
Tyson-07 <72865445+Tyson-07@users.noreply.github.com>
2020-12-05 19:06:07 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,31 @@
+class Bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+    }
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y,this.r, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			rectMode(CENTER)
+			fill("pink")
+			ellipse(0,0,this.r,this.r);
+			pop()
+			
+  }
+}
